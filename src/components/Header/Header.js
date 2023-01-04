7832311fd2e2569eab32b656663eef3bc0ef603f
Alex <eddings.sharanna@gmail.com>
2023-01-04 12:29:12 -0500

    imported Footer component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import LeftSection from "../../components/LeftSection";
+import RightSection from "../RightSection";
+import Footer from "../Footer";
 
 export default function Header() {
   return (
     <>
       <LeftSection />
+      <RightSection />
+      <Footer />
     </>
   );
 }
