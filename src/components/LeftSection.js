48a51162193aa926caf345e161312bbc7d644dd5
Alex <eddings.sharanna@gmail.com>
2023-01-04 12:32:11 -0500

    updated Left Section

--- src/components/LeftSection.js
+++ src/components/LeftSection.js
@@ -1,32 +1,10 @@
+// components
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
 
 export default class LeftSection extends Component {
   render() {
-    const images = [
-      {
-        name: "Local Green Atlanta",
-        item: "Vans Old Skool",
-        price: "74.99",
-        img: "https://i.imgur.com/x9R5321.png",
-        alt: "Foot The Bill customs Vans for business Local Green Atlanta, located in Atlanta, Georgia",
-      },
-      {
-        name: "Black Genius Art Show",
-        item: "Vans Classic Slip-On",
-        price: "74.99",
-        img: "https://i.imgur.com/jiB0r8U.png",
-        alt: "Foot The Bill customs Vans for business Eubie Blake Cultural Center, located in Baltimore, Maryland",
-      },
-      {
-        name: "Milk + T",
-        item: "Vans Authentic Canvas",
-        price: "74.99",
-        img: "../../src/img/ftb-portland-milk-t-shoe.webp",
-        alt: "Foot The Bill customs Vans for business Milk T, located in Portland, Oregon",
-      },
-    ];
     return (
       <>
         <div className="hero">
@@ -36,29 +14,11 @@ export default class LeftSection extends Component {
                 <h2>Online</h2>
                 <h1>CUSTOM VANS</h1>
                 <h3>STORE</h3>
-                <a
-                  href="https://www.vans.com/en-us/customs?icn=topnav"
-                  target="_blank"
-                  class="btn"
-                  rel="noreferrer"
-                >
-                  Buy Now
-                </a>
-              </div>
 
-              <Carousel className="carousel">
-                {images.map((image) => {
-                  const { img, alt, price } = image;
-                  return (
-                    <>
-                      <div>
-                        <img src={`${img}`} alt={`${alt}`} />
-                        <p className="legend">{`${price}`}</p>
-                      </div>
-                    </>
-                  );
-                })}
-              </Carousel>
+                <Link to="/orders/new" className="btn">
+                  Buy Now
+                </Link>
+              </div>
             </div>
           </div>
         </div>
