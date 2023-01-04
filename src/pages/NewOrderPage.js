904046ade095f540715ad9f2a4fd951702e4137b
Alex <eddings.sharanna@gmail.com>
2023-01-04 12:25:36 -0500

    added order page

---
+++ src/pages/NewOrderPage.js
@@ -0,0 +1,11 @@
+export default function NewOrderPage() {
+  return (
+    <>
+      <div className="newOrder">
+        <div className="newOrder-container">
+          <h1>New Order Page</h1>
+        </div>
+      </div>
+    </>
+  );
+}
