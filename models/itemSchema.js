b56b11ecba9efaa9e2d39a0fe1bdae935c728caf
Alex <eddings.sharanna@gmail.com>
2023-01-04 19:25:35 -0500

    updated item schema

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -5,6 +5,7 @@ const Schema = require("mongoose").Schema;
 const itemSchema = new Schema(
   {
     name: { type: String, required: true },
+    img: String,
     category: { type: Schema.Types.ObjectId, ref: "Category" },
     price: { type: Number, required: true, default: 0 },
   },
